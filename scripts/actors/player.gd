5b8d62d9b595a5bb1fe42e70a4a055b2a2340c4a
Alix <alix@github.com>
2024-03-28 19:59:41 +0100

    Feat: Add Player run / walk
    Also refactor the inputs a bit

--- scripts/actors/player.gd
+++ scripts/actors/player.gd
@@ -2,9 +2,6 @@ extends CharacterBody2D
 class_name Player
 
 
-# ----------------- DECLARE VARIABLES -----------------
-
-
 var direction: Vector2 = Vector2(1.0, 0.0)
 
 
@@ -12,52 +9,48 @@ var gravity: float = 10.0
 var max_fall_speed: float = 350.0
 
 
-var max_move_speed: float = 100.0
-# Factor to gain gradual move acceleration
+var move_speed: float = 0.0
+var max_walk_speed: float = 250.0
+var max_run_speed: float = 500.0
+## Factor to gain gradual move acceleration
 var move_acceleration_amount: float = 20.0
-# Factor to loose gradual move acceleration
-var move_friction_amount: float = 10.0
+## Factor to loose gradual move acceleration
+var move_friction_amount: float = 30.0
 
 var gravity_multiplier_factor: float = 8.0
-var movement_input: float = 0.0
 var friction: float = 0.0
 
 var min_jump_force: float = -50.0
 var jump_force: float = -250
 
 
-# ----------------- RUN CODE -----------------
+var movement_input: Vector2 = Vector2(0.0, 0.0)
+var is_secondary_action_pressed: bool = false
+
+func _unhandled_key_input(_event: InputEvent) -> void:
+	self.movement_input.x = Input.get_axis("move_left", "move_right")
+	is_secondary_action_pressed = Input.is_action_pressed("action_secondary")
 
 
 func _physics_process(_delta: float) -> void:
 	if not self.is_on_floor():
 		self.apply_gravity()
 		
-		if Input.is_action_just_released("jump") and velocity.y < self.min_jump_force:
+		if Input.is_action_just_released("action_primary") and velocity.y < self.min_jump_force:
 			self.velocity.y = self.min_jump_force
-	
 	else:
-		if Input.is_action_just_pressed("jump"):
+		if Input.is_action_just_pressed("action_primary"):
 			self.velocity.y = self.jump_force
 	
-
-	self.movement_input = Input.get_axis("move_left", "move_right")
-	self.direction.x = self.movement_input
 	
-	if self.movement_input == 0:
+	self.direction.x = self.movement_input.x
+	
+	if self.movement_input.x == 0:
 		self.apply_movement_friction()
 	else:
-		self.apply_movement_acceleration(self.movement_input)
+		self.apply_movement_acceleration()
 	
-
 	self.move_and_slide()
-
-
-func _unhandled_input(_event: InputEvent) -> void:
-	return
-
-
-# ----------------- DECLARE FUNCTIONS -----------------
 
 
 func apply_gravity() -> void:
@@ -70,8 +63,13 @@ func apply_gravity() -> void:
 		self.velocity.y = self.max_fall_speed
 
 
-func apply_movement_acceleration(controller_input: float) -> void:
-	self.velocity.x = move_toward(self.velocity.x, (self.max_move_speed * controller_input), self.move_acceleration_amount)
+func apply_movement_acceleration() -> void:
+	if is_secondary_action_pressed:
+		self.move_speed = self.max_run_speed
+	else:
+		self.move_speed = self.max_walk_speed
+	
+	self.velocity.x = move_toward(self.velocity.x, (self.move_speed * self.movement_input.x), self.move_acceleration_amount)
 
 
 func apply_movement_friction() -> void:
