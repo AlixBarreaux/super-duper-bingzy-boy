5c44c1e0ae41cca1ab04108a80b2cd15b4953974
Alix <alix@github.com>
2024-03-29 21:53:13 +0100

    Feat: Update lives counter

--- scripts/ui/lives_counter_ui.gd
+++ scripts/ui/lives_counter_ui.gd
@@ -2,15 +2,20 @@ extends HBoxContainer
 class_name LivesCounterUI
 
 
+@onready var game: Game = get_tree().get_root().get_node("Game")
+var level: Level = null
+var stats: Stats = null
 @onready var label: Label = $Label
 
 
-func on_lives_changed() -> void:
-	#self.label.set_text("x " + str(Global.lives))
-	pass
+func on_lives_changed(value: int) -> void:
+	self.label.set_text("x " + str(value))
 
 
 func _ready() -> void:
-	#self.label.set_text("x " + str(Global.lives))
-	#Global.lives_changed.connect(on_lives_changed)
-	pass
+	await get_tree().get_root().ready
+	level = game.level
+	stats = game.stats
+	
+	self.label.set_text("x " + str(stats.lives))
+	stats.lives_changed.connect(on_lives_changed)
