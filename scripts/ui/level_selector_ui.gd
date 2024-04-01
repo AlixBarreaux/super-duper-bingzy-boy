96eb70a92b22c4f33ea914c1c89d79002896612a
Alix <alix@github.com>
2024-04-01 22:32:08 +0200

    Refactor: export name + pressed signal
    Use an export var with a file name so if the levels are moved to another directory the only var to change is the one containing the directory path.
    
    Also use the pressed signal of the Button class to click instead of using _gui_input() with mouse events.

--- scripts/ui/level_selector_ui.gd
+++ scripts/ui/level_selector_ui.gd
@@ -2,17 +2,20 @@ extends Button
 class_name LevelSelectorUI
 
 
-@export_file var level_to_load_file_path: String = ""
+const LEVELS_DIRECTORY: String = "res://scenes/levels/"
+@export var next_level_to_load_file_name: String = ""
+@onready var next_level_to_load_file_path: String = LEVELS_DIRECTORY + next_level_to_load_file_name + ".tscn"
 
 signal level_selected(level_file_path: String)
 
 
-func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-			accept_event()
-			level_selected.emit(level_to_load_file_path)
-
-
 func _ready() -> void:
-	assert(FileAccess.file_exists(level_to_load_file_path), "The Level to load file path is invalid!")
+	assert(next_level_to_load_file_name != "", "Value not provided!")
+	print("name: ", next_level_to_load_file_name)
+	print(next_level_to_load_file_path)
+	assert(FileAccess.file_exists(next_level_to_load_file_path), "The Level to load file path is invalid!")
+
+
+func _on_pressed() -> void:
+	accept_event()
+	level_selected.emit(next_level_to_load_file_path)
