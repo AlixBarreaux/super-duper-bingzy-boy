dfa828629f8bbcbb87689797957e94764b4e507c
Alix <alix@github.com>
2024-04-01 02:00:37 +0200

    Fix: Camera not following Player
    Null ref on focus_point changed

--- scripts/camera_main.gd
+++ scripts/camera_main.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 
 @export var actor_to_follow: Node2D = null
-var focus_point: Node2D = null
+@onready var focus_point: Node2D = actor_to_follow.camera_focus_point
 
 @export var level_boundaries: LevelBoundaries = null
 @onready var level_boundary_left: CollisionShape2D = level_boundaries.boundary_left
@@ -13,9 +13,6 @@ func _ready() -> void:
 	assert(actor_to_follow != null, "actor_to_follow must bet set!")
 	assert(level_boundaries != null, "level_boundaries must be set!")
 	set_physics_process(false)
-	
-	await actor_to_follow.ready
-	focus_point = actor_to_follow.camera_focus_point
 	
 	self.set_limit(SIDE_LEFT, int(level_boundary_left.get_global_position().x))
 	self.set_limit(SIDE_RIGHT, int(level_boundary_right.get_global_position().x))
